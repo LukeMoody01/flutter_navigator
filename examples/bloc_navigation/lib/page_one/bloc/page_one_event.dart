8d980c5815c9d0ffc657b664e5d21715965862c8
Luke Moody <luke.moody@xam-consulting.com>
2022-06-15 10:19:50 +1000

    chore(flutter_navigator): Update documentation and remove bad practices example

--- examples/bloc_navigation/lib/page_one/bloc/page_one_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'page_one_bloc.dart';
-
-@immutable
-abstract class PageOneEvent {}
-
-class NavigateToPageTwoEvent extends PageOneEvent {}
--- examples/bloc_navigation/lib/page_one/bloc/page_one_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'page_one_bloc.dart';
-
-@immutable
-abstract class PageOneState {}
-
-class PageoneInitial extends PageOneState {}
